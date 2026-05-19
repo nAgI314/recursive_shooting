c18f38af867e63cb2345d1b8c94833d9b71c4c1e
nAgI314 <kurosawasyunnagi@gmail.com>
2026-05-19 19:52:30 +0900

    動く

--- src/pages/home/components/pixel.tsx
+++ src/pages/home/components/pixel.tsx
@@ -1,14 +1,67 @@
+import { useEffect, useState } from "react"
 import type { Coordinate } from "../lib/type"
 import { Character } from "./character"
 import styles from "./pixel.module.scss"
 
-export const Pixel = (position:Coordinate) => {
+type PixelProps = {
+    firstPosition: Coordinate
+    isPlayer?: boolean
+}
+
+export const Pixel = ({ firstPosition, isPlayer }: PixelProps) => {
+    const [pos, setPos] = useState(firstPosition);
+
+    // キー入力での移動処理
+    useEffect(() => {
+        if (!isPlayer) return;
+        const handleKeyDown = (evt: KeyboardEvent) => {
+            evt.preventDefault();
+
+            switch (evt.key) {
+                case "ArrowUp":
+                    setPos((prev) => ({
+                        ...prev,
+                        y: prev.y > 1 ? prev.y - 1 : prev.y,
+                    }));
+                    break;
+
+                case "ArrowDown":
+                    setPos((prev) => ({
+                        ...prev,
+                        y: prev.y < 10 ? prev.y + 1 : prev.y,
+                    }));
+                    break;
+
+                case "ArrowLeft":
+                    setPos((prev) => ({
+                        ...prev,
+                        x: prev.x > 1 ? prev.x - 1 : prev.x,
+                    }));
+                    break;
+
+                case "ArrowRight":
+                    setPos((prev) => ({
+                        ...prev,
+                        x: prev.x < 10 ? prev.x + 1 : prev.x,
+                    }));
+                    break;
+            }
+        };
+
+        document.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
+
+
     return (
         <div className={styles.pixel} style={{
-        gridRow: `${position.y}`,
-        gridColumn: `${position.x}`,
-      }}>
-            <Character/>
+            gridRow: `${pos.y}`,
+            gridColumn: `${pos.x}`,
+        }}>
+            <Character />
         </div>
     )
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,10 +2,12 @@ import { Pixel } from "./components/pixel"
 import styles from "./index.module.scss"
 
 const Home = () => {
+    const firstPosition = { x: 3, y: 2 }
     return (
         <div className={styles.layout}>
             <div className={styles.stage}>
-                <Pixel x={4} y={3}/>
+                <Pixel firstPosition={firstPosition} isPlayer={true} />
+                <Pixel firstPosition={firstPosition} isPlayer={false} />
             </div>
         </div>
     )
